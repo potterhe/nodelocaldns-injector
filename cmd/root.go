4eeb9d718366ea524ece348170a1ee896a1dc127
potterhe <potter.he@gmail.com>
2020-06-15 09:19:08 +0800

    cobra add serve

--- cmd/root.go
+++ cmd/root.go
@@ -17,8 +17,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -29,13 +30,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "nodelocaldns-injector",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Kubernetes webhook for automatic dnsConfig injection.",
+	Long:  `Kubernetes webhook for automatic dnsConfig injection.`,
+	// https://github.com/spf13/cobra#version-flag
+	Version: "1.0.0",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
